4fa734f21c07a4222e65c0b666fcee8b689e989c
TTT999 <thientu.09072009@gmail.com>
2026-08-16 05:21:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use(express.json({ limit: "1mb" }));
-app.use(express.static(__dirname));
 app.use((req,res,next)=>{
   res.setHeader("Access-Control-Allow-Origin","*");
   res.setHeader("Access-Control-Allow-Headers","Content-Type, Authorization, x-admin-key");
